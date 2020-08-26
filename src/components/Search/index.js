16e863cce79f82081ae63365b8178d1d6edba49d
phucNguyen <namphuc.96@gmail.com>
2020-08-26 20:53:10 +0700

    Fix bug search users.

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -11,29 +11,33 @@ import {
 } from "../../redux/actions/github";
 
 const SearchUser = (props) => {
-  const _handleSearchText = useCallback((e) => {
-    const { value } = e.target;
-    debounced(async () => {
-      actionUserSearch(value);
-    });
+  const _handleSearchText = useCallback((e, data) => {
+    if (data.value)
+      debounced(async () => {
+        actionUserSearch(data.value);
+      });
   }, []);
 
-  const _handleResultSelect = useCallback(async (e, data) => {
-    if (data.results?.[0]?.login) {
-      const username = data.results[0].login;
-      actionSetLoading(true);
-      await Promise.all([
-        actionSetCurrentUser(data.results[0]),
-        actionRepoSearch(username),
-        actionOrganizationSearch(username),
-      ]);
+  const _handleResultSelect = useCallback(
+    (item) => async () => {
+      try {
+        if (item) {
+          actionSetLoading(true);
+          await Promise.all([
+            actionSetCurrentUser(item),
+            actionRepoSearch(item.login),
+            actionOrganizationSearch(item.login),
+          ]);
+        }
+      } catch (e) {}
       actionSetLoading(false);
-    }
-  }, []);
+    },
+    []
+  );
 
   const _resultRenderer = (item) => {
     return (
-      <Item>
+      <Item onClick={_handleResultSelect(item)} key={item.id}>
         <Item.Image size="tiny" src={item.avatar_url} />
         <Item.Content>
           <Item.Header as="a">{item.login}</Item.Header>
@@ -47,7 +51,6 @@ const SearchUser = (props) => {
       width={12}
       placeholder="Search github"
       loading={props.isSearching}
-      onResultSelect={_handleResultSelect}
       onSearchChange={_handleSearchText}
       resultRenderer={_resultRenderer}
       results={props.users}
